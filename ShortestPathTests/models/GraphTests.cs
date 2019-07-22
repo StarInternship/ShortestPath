2b8818f1a56ea60ce196dbb4531c0cc1e45a34f8
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-22 20:09:24 +0430

    refactor tests

--- ShortestPathTests/models/GraphTests.cs
+++ ShortestPathTests/models/GraphTests.cs
@@ -7,17 +7,16 @@ namespace ShortestPath.models.Tests
     public class GraphTests
     {
         [TestMethod()]
-        public void CreatePathTest()
+        public void SimpleGraphSearch()
         {
-            {
-                Graph graph = new Graph(4);
-                graph.AddEdge(0, 1, 1);
-                graph.AddEdge(1, 2, 2);
-                graph.AddEdge(1, 3, 3);
-                graph.AddEdge(2, 3, 1);
+            Graph graph = new Graph(4);
+            graph.AddEdge(0, 1, 1);
+            graph.AddEdge(1, 2, 2);
+            graph.AddEdge(1, 3, 3);
+            graph.AddEdge(2, 3, 1);
 
-                var actual = graph.FindPath(0, 3);
-                var expected = new List<Path>
+            var actual = graph.FindPath(0, 3);
+            var expected = new List<Path>
                 {
                     new Path
                     {
@@ -29,25 +28,27 @@ namespace ShortestPath.models.Tests
                     }
                 };
 
-                Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.Count, actual.Count);
 
-                for (int i = 0; i < expected.Count; i++)
-                {
-                    CollectionAssert.AreEqual(expected[i], actual[i]);
-                }
-            }
-
+            for (int i = 0; i < expected.Count; i++)
             {
-                Graph graph = new Graph(5);
-                graph.AddEdge(0, 1, 1);
-                graph.AddEdge(0, 2, 1);
-                graph.AddEdge(0, 3, 2);
-                graph.AddEdge(2, 3, 1);
-                graph.AddEdge(1, 3, 1);
-                graph.AddEdge(3, 4, 1);
+                CollectionAssert.AreEqual(expected[i], actual[i]);
+            }
+        }
 
-                var actual = graph.FindPath(0, 4);
-                var expected = new List<Path>
+        [TestMethod()]
+        public void ALittleComplicatedGraphSearch()
+        {
+            Graph graph = new Graph(5);
+            graph.AddEdge(0, 1, 1);
+            graph.AddEdge(0, 2, 1);
+            graph.AddEdge(0, 3, 2);
+            graph.AddEdge(2, 3, 1);
+            graph.AddEdge(1, 3, 1);
+            graph.AddEdge(3, 4, 1);
+
+            var actual = graph.FindPath(0, 4);
+            var expected = new List<Path>
                 {
                     new Path
                     {
@@ -63,24 +64,26 @@ namespace ShortestPath.models.Tests
                     }
                 };
 
-                Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.Count, actual.Count);
 
-                for (int i = 0; i < expected.Count; i++)
-                {
-                    CollectionAssert.AreEqual(expected[i], actual[i]);
-                }
-            }
-
+            for (int i = 0; i < expected.Count; i++)
             {
-                Graph graph = new Graph(5);
-                graph.AddEdge(0, 2, 1);
-                graph.AddEdge(0, 1, 10);
-                graph.AddEdge(2, 1, 1);
-                graph.AddEdge(1, 3, 1);
-                graph.AddEdge(3, 4, 20);
+                CollectionAssert.AreEqual(expected[i], actual[i]);
+            }
+        }
 
-                var actual = graph.FindPath(0, 4);
-                var expected = new List<Path>
+        [TestMethod()]
+        public void VisitedGraphSearch()
+        {
+            Graph graph = new Graph(5);
+            graph.AddEdge(0, 2, 1);
+            graph.AddEdge(0, 1, 10);
+            graph.AddEdge(2, 1, 1);
+            graph.AddEdge(1, 3, 1);
+            graph.AddEdge(3, 4, 20);
+
+            var actual = graph.FindPath(0, 4);
+            var expected = new List<Path>
                 {
                     new Path
                     {
@@ -88,14 +91,12 @@ namespace ShortestPath.models.Tests
                     }
                 };
 
-                Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.Count, actual.Count);
 
-                for (int i = 0; i < expected.Count; i++)
-                {
-                    CollectionAssert.AreEqual(expected[i], actual[i]);
-                }
+            for (int i = 0; i < expected.Count; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], actual[i]);
             }
-
         }
     }
 }
