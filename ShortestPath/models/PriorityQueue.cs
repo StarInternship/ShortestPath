0edba60f50dc5708d24718747a69d47ab71cd52a
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-22 12:19:34 +0430

    add PriorityQueue class

---
+++ ShortestPath/models/PriorityQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ShortestPath.models
+{
+    public class PriorityQueue : LinkedList<Node>
+    {
+        public void Add(Node node)
+        {
+
+        }
+
+        public Node Pop()
+        {
+            return new Node(0);
+        }
+    }
+}
