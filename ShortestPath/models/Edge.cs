2ca2efeaf54987b801494dde47ba449f469867d2
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-22 11:47:20 +0430

    add Node and Edge classes

---
+++ ShortestPath/models/Edge.cs
@@ -0,0 +1,9 @@
+﻿namespace ShortestPath.models
+{
+    internal class Edge
+    {
+        public Node From { get; }
+        public Node To { get; }
+        public double weight { get; }
+    }
+}
---
+++ ShortestPath/models/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestPath.models
+{
+    class Node
+    {
+        public int Index { get; }
+        public LinkedList<Edge> Outs { get; } = new LinkedList<Edge>();
+        public bool Visited { get; set; }
+        public double Distance { get; set; } = Double.MaxValue;
+    }
+}
