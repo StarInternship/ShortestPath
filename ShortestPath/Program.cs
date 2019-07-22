a3fe6ac82e0862a94aa3918595a80a820a56b53c
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-22 16:45:48 +0430

    refctor

--- ShortestPath/Program.cs
+++ ShortestPath/Program.cs
@@ -1,37 +1,35 @@
 ﻿using ShortestPath.models;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShortestPath
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Graph graph = new GraphReader().Read(@"C:\Graph\hadi");
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Graph graph = new GraphReader().Read(@"../../../TestFiles/hossein_test");
+            Console.WriteLine("graph read done. duration: " + stopwatch.ElapsedMilliseconds + " ms.");
             while (true)
             {
                 Console.Write("from: ");
-                int src = int.Parse(Console.ReadLine());
+                int source = int.Parse(Console.ReadLine());
                 Console.Write("dest: ");
-                int dest = int.Parse(Console.ReadLine());
+                int destination = int.Parse(Console.ReadLine());
+
                 stopwatch.Restart();
-                var path = graph.FindPath(src, dest);
+                var path = graph.FindPath(source, destination);
+
                 if (path.Equals(Path.NOT_FOUND))
                 {
-                    Console.WriteLine("not found " + stopwatch.ElapsedMilliseconds);
+                    Console.WriteLine("not found. time: " + stopwatch.ElapsedMilliseconds + " ms.");
                 }
                 else
                 {
-                    Console.WriteLine("find a path in " + stopwatch.ElapsedMilliseconds + " with " + path.Distance);
-                    Console.Write("(" + src + ")");
+                    Console.WriteLine("find a path in " + stopwatch.ElapsedMilliseconds + " ms with distance of " + path.Distance);
+                    Console.Write("(" + source + ")");
                     foreach (Edge edge in path)
                     {
                         Console.Write(" -" + edge.Weight + "-> (" + edge.To + ")");
