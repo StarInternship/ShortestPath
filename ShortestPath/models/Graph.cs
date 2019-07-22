43191518a27704b4872d36939cf056651d2f4566
hadi <atomaton00@gmail.com>
2019-07-22 13:56:21 +0430

    پیاده سازی تابع FindPath در گراف

--- ShortestPath/models/Graph.cs
+++ ShortestPath/models/Graph.cs
@@ -1,4 +1,6 @@
-﻿namespace ShortestPath.models
+﻿using System.Collections.Generic;
+
+namespace ShortestPath.models
 {
     class Graph
     {
@@ -17,7 +19,38 @@
 
         public Path FindePath(Node source, Node target)
         {
-            return new Path();
+            source.Distance = 0;
+
+            PriorityQueue nodes = new PriorityQueue();
+
+            nodes.Add(source);
+
+            while (nodes.Count > 0)
+            {
+                Node node = nodes.Pop();
+
+                if (node.Visited) continue;
+                node.Visited = true;
+
+                foreach (Edge edge in node.Outs)
+                {
+                    if (target.Distance > node.Distance + edge.Weight && node.Distance + edge.Weight < edge.To.Distance)
+                    {
+                        nodes.Add(edge.To);
+                        edge.To.Distance = node.Distance + edge.Weight;
+                        edge.To.LastInEdge = edge;
+                    }
+                }
+            }
+            Path path = new Path();
+
+            Edge lastEdge = target.LastInEdge;
+            while (lastEdge.From.Index != source.Index)
+            {
+                path.Add(lastEdge);
+                lastEdge = lastEdge.From.LastInEdge;
+            }
+            return path;
         }
     }
 }
