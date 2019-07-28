a5ede1a29db4174342d15f27bf934d4f2e3f9af0
ahmadsalimi <ahsa9978@gmail.com>
2019-07-28 14:51:21 +0430

    add xmldoc to ResultWriter.cs

--- ShortestPath/models/ResultWriter.cs
+++ ShortestPath/models/ResultWriter.cs
@@ -2,12 +2,42 @@
 
 namespace ShortestPath
 {
-    public static class ResultWriter
+    /// <summary>
+    /// a class thar writes the result graph in .csv format
+    /// </summary>
+    public class ResultWriter
     {
+        /// <summary>
+        /// relative path of results directory
+        /// </summary>
         private static readonly string resultPath = "../../../SearchResults/";
-        public static void WriteLine(string fileName, object text)
+        /// <summary>
+        /// .csv format
+        /// </summary>
+        private static readonly string format = ".csv";
+        /// <summary>
+        /// relative path to file
+        /// </summary>
+        private readonly string filePath;
+
+        /// <summary>
+        /// recreates the file.
+        /// </summary>
+        /// <param name="fileName">name of the result file(without format)</param>
+        public ResultWriter(string fileName)
         {
-            File.AppendAllText(resultPath + fileName, text + "\n");
+            filePath = resultPath + fileName + format;
+            File.Delete(filePath);
+            File.Create(filePath);
+        }
+
+        /// <summary>
+        /// writes a line in result file
+        /// </summary>
+        /// <param name="obj">object</param>
+        public void WriteLine(object obj)
+        {
+            File.AppendAllText(filePath, obj + "\n");
         }
     }
 }
