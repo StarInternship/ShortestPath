fb306d9b02ac68655db34d9c56f611e7beb06a68
ahmadsalimi <ahsa9978@gmail.com>
2019-08-01 11:39:18 +0430

    add Layout classes

---
+++ GraphVisualization/Models/BccCompact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GraphVisualization.Models
+{
+    public class BccCompact : ILayout
+    {
+        private readonly GraphContainer graph;
+        private readonly List<Component> components = new List<Component>();
+        public BccCompact(GraphContainer graph)
+        {
+            this.graph = graph;
+        }
+
+        public void ApplyLayout()
+        {
+            
+        }
+
+        private void MakeComponents()
+        {
+
+        }
+
+        private void ArrangeComponents()
+        {
+
+        }
+    }
+}
---
+++ GraphVisualization/Models/Component.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphVisualization.Models
+{
+    public class Component
+    {
+        public List<NodeContainer> Nodes { get; } = new List<NodeContainer>();
+        public List<EdgeContainer> OuterEdges { get; } = new List<EdgeContainer>();
+        public int CenterX { get; set; }
+        public int CenterY { get; set; }
+        public int Radius { get; set; }
+
+        public void ArrangeNodes()
+        {
+
+        }
+    }
+}
